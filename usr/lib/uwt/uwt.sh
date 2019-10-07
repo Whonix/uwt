be7d51ef0f1baba716a6ce69c47143438f268825
Patrick Schleizer <adrelanos@riseup.net>
2019-10-07 09:16:19 +0000

    code documentation

--- usr/lib/uwt/uwt.sh
+++ usr/lib/uwt/uwt.sh
@@ -3,7 +3,14 @@
 ## Copyright (C) 2012 - 2018 ENCRYPTED SUPPORT LP <adrelanos@riseup.net>
 ## See the file COPYING for copying conditions.
 
+#### meta start
+#### project Whonix
+#### category networking
+#### description
+
 ## Disable torsocks warning spam such as.
 ## [May 20 11:45:27] WARNING torsocks[2645]: [syscall] Unsupported syscall number 224. Denying the call (in tsocks_syscall() at syscall.c:165)
 ## https://phabricator.whonix.org/T317
 export TORSOCKS_LOG_LEVEL=1
+
+#### meta end
